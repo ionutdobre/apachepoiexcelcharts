b5339d102139ca83c5c8644495a0b34c3a6f023b
Ionut Dobre <dobreionut88@gmail.com>
2016-08-09 16:18:38 -0400

    added pie chart.

--- src/main/java/org/excelchart/Main.java
+++ src/main/java/org/excelchart/Main.java
@@ -14,6 +14,8 @@ import org.apache.poi.ss.usermodel.charts.ChartDataSource;
 import org.apache.poi.ss.usermodel.charts.ChartLegend;
 import org.apache.poi.ss.usermodel.charts.DataSources;
 import org.apache.poi.ss.usermodel.charts.LegendPosition;
+import org.apache.poi.ss.usermodel.charts.LineChartData;
+import org.apache.poi.ss.usermodel.charts.ScatterChartData;
 import org.apache.poi.ss.usermodel.charts.ValueAxis;
 import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -21,6 +23,8 @@ import org.excelchart.areachart.AreaChartData;
 import org.excelchart.areachart.XSSFAreaChartData;
 import org.excelchart.barchart.BarChartData;
 import org.excelchart.barchart.XSSFBarChartData;
+import org.excelchart.piechart.PieChartData;
+import org.excelchart.piechart.XSSFPieChartData;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -38,17 +42,18 @@ public class Main {
         System.out.println(">>> start");
 
         try {
-            // createScatterChart();
-            // createLineChart();
+            createScatterChart();
+            createLineChart();
             createBarChart();
             createAreaChartChart();
+            createPieChartChart();
         } catch (IOException e) {
             e.printStackTrace();
         }
         System.out.println(">>> end");
     }
 
-    /*
+
     private static void createScatterChart() throws IOException {
         Workbook wb = new XSSFWorkbook();
         Sheet sheet = wb.createSheet("Sheet 1");
@@ -60,7 +65,11 @@ public class Main {
             row = sheet.createRow((short) rowIndex);
             for (int colIndex = 0; colIndex < NUM_OF_COLUMNS; colIndex++) {
                 cell = row.createCell((short) colIndex);
-                cell.setCellValue(getRandomNumberInRange(1, 10));
+                if (rowIndex == 0) {
+                    cell.setCellValue(colIndex + 1);
+                } else {
+                    cell.setCellValue(getRandomNumberInRange(1, 10));
+                }
             }
         }
 
@@ -90,51 +99,55 @@ public class Main {
         FileOutputStream fileOut = new FileOutputStream("/Users/ionut/Downloads/ooxml-scatter-chart.xlsx");
         wb.write(fileOut);
         fileOut.close();
-    }*/
+    }
 
-    // private static void createLineChart() throws IOException {
-    //     Workbook wb = new XSSFWorkbook();
-    //     Sheet sheet = wb.createSheet("linechart");
-    //
-    //     // Create a row and put some cells in it. Rows are 0 based.
-    //     Row row;
-    //     Cell cell;
-    //     for (int rowIndex = 0; rowIndex < NUM_OF_ROWS; rowIndex++) {
-    //         row = sheet.createRow((short) rowIndex);
-    //         for (int colIndex = 0; colIndex < NUM_OF_COLUMNS; colIndex++) {
-    //             cell = row.createCell((short) colIndex);
-    //             cell.setCellValue(getRandomNumberInRange(1, 10));
-    //         }
-    //     }
-    //
-    //     Drawing drawing = sheet.createDrawingPatriarch();
-    //     ClientAnchor anchor = drawing.createAnchor(0, 0, 0, 0, 0, 5, 15, 20);
-    //
-    //     Chart chart = drawing.createChart(anchor);
-    //     ChartLegend legend = chart.getOrCreateLegend();
-    //     legend.setPosition(LegendPosition.TOP_RIGHT);
-    //
-    //     LineChartData data = chart.getChartDataFactory().createLineChartData();
-    //
-    //     // Use a category axis for the bottom axis.
-    //     ChartAxis bottomAxis = chart.getChartAxisFactory().createCategoryAxis(AxisPosition.BOTTOM);
-    //     ValueAxis leftAxis = chart.getChartAxisFactory().createValueAxis(AxisPosition.LEFT);
-    //     leftAxis.setCrosses(AxisCrosses.AUTO_ZERO);
-    //
-    //     ChartDataSource<Number> xs = DataSources.fromNumericCellRange(sheet, new CellRangeAddress(0, 0, 0, NUM_OF_COLUMNS - 1));
-    //     ChartDataSource<Number> ys1 = DataSources.fromNumericCellRange(sheet, new CellRangeAddress(1, 1, 0, NUM_OF_COLUMNS - 1));
-    //     ChartDataSource<Number> ys2 = DataSources.fromNumericCellRange(sheet, new CellRangeAddress(2, 2, 0, NUM_OF_COLUMNS - 1));
-    //
-    //     data.addSeries(xs, ys1);
-    //     data.addSeries(xs, ys2);
-    //
-    //     chart.plot(data, bottomAxis, leftAxis);
-    //
-    //     // Write the output to a file
-    //     FileOutputStream fileOut = new FileOutputStream("/Users/ionut/Downloads/ooxml-line-chart.xlsx");
-    //     wb.write(fileOut);
-    //     fileOut.close();
-    // }
+    private static void createLineChart() throws IOException {
+        Workbook wb = new XSSFWorkbook();
+        Sheet sheet = wb.createSheet("linechart");
+
+        // Create a row and put some cells in it. Rows are 0 based.
+        Row row;
+        Cell cell;
+        for (int rowIndex = 0; rowIndex < NUM_OF_ROWS; rowIndex++) {
+            row = sheet.createRow((short) rowIndex);
+            for (int colIndex = 0; colIndex < NUM_OF_COLUMNS; colIndex++) {
+                cell = row.createCell((short) colIndex);
+                if (rowIndex == 0) {
+                    cell.setCellValue(colIndex + 1);
+                } else {
+                    cell.setCellValue(getRandomNumberInRange(1, 10));
+                }
+            }
+        }
+
+        Drawing drawing = sheet.createDrawingPatriarch();
+        ClientAnchor anchor = drawing.createAnchor(0, 0, 0, 0, 0, 5, 15, 20);
+
+        Chart chart = drawing.createChart(anchor);
+        ChartLegend legend = chart.getOrCreateLegend();
+        legend.setPosition(LegendPosition.TOP_RIGHT);
+
+        LineChartData data = chart.getChartDataFactory().createLineChartData();
+
+        // Use a category axis for the bottom axis.
+        ChartAxis bottomAxis = chart.getChartAxisFactory().createCategoryAxis(AxisPosition.BOTTOM);
+        ValueAxis leftAxis = chart.getChartAxisFactory().createValueAxis(AxisPosition.LEFT);
+        leftAxis.setCrosses(AxisCrosses.AUTO_ZERO);
+
+        ChartDataSource<Number> xs = DataSources.fromNumericCellRange(sheet, new CellRangeAddress(0, 0, 0, NUM_OF_COLUMNS - 1));
+        ChartDataSource<Number> ys1 = DataSources.fromNumericCellRange(sheet, new CellRangeAddress(1, 1, 0, NUM_OF_COLUMNS - 1));
+        ChartDataSource<Number> ys2 = DataSources.fromNumericCellRange(sheet, new CellRangeAddress(2, 2, 0, NUM_OF_COLUMNS - 1));
+
+        data.addSeries(xs, ys1);
+        data.addSeries(xs, ys2);
+
+        chart.plot(data, bottomAxis, leftAxis);
+
+        // Write the output to a file
+        FileOutputStream fileOut = new FileOutputStream("/Users/ionut/Downloads/ooxml-line-chart.xlsx");
+        wb.write(fileOut);
+        fileOut.close();
+    }
 
     private static void createBarChart() throws IOException {
         Workbook wb = new XSSFWorkbook();
@@ -147,7 +160,11 @@ public class Main {
             row = sheet.createRow((short) rowIndex);
             for (int colIndex = 0; colIndex < NUM_OF_COLUMNS; colIndex++) {
                 cell = row.createCell((short) colIndex);
-                cell.setCellValue(getRandomNumberInRange(1, 10));
+                if (rowIndex == 0) {
+                    cell.setCellValue(colIndex + 1);
+                } else {
+                    cell.setCellValue(getRandomNumberInRange(1, 10));
+                }
             }
         }
 
@@ -180,9 +197,6 @@ public class Main {
         fileOut.close();
     }
 
-    // plotArea.addNewDoughnutChart();
-    // plotArea.addNewPieChart();
-
     private static void createAreaChartChart() throws IOException {
         Workbook wb = new XSSFWorkbook();
         Sheet sheet = wb.createSheet("areachart");
@@ -194,7 +208,11 @@ public class Main {
             row = sheet.createRow((short) rowIndex);
             for (int colIndex = 0; colIndex < NUM_OF_COLUMNS; colIndex++) {
                 cell = row.createCell((short) colIndex);
-                cell.setCellValue(getRandomNumberInRange(1, 10));
+                if (rowIndex == 0) {
+                    cell.setCellValue(colIndex + 1);
+                } else {
+                    cell.setCellValue(getRandomNumberInRange(1, 10));
+                }
             }
         }
 
@@ -224,6 +242,48 @@ public class Main {
         wb.write(fileOut);
         fileOut.close();
     }
+
+    private static void createPieChartChart() throws IOException {
+        Workbook wb = new XSSFWorkbook();
+        Sheet sheet = wb.createSheet("piechart");
+
+        // Create a row and put some cells in it. Rows are 0 based.
+        Row row;
+        Cell cell;
+        for (int rowIndex = 0; rowIndex < NUM_OF_ROWS; rowIndex++) {
+            row = sheet.createRow((short) rowIndex);
+            for (int colIndex = 0; colIndex < NUM_OF_COLUMNS - 10; colIndex++) {
+                cell = row.createCell((short) colIndex);
+                if (rowIndex == 0) {
+                    cell.setCellValue(colIndex + 1);
+                } else {
+                    cell.setCellValue(getRandomNumberInRange(1, 10));
+                }
+            }
+        }
+
+        Drawing drawing = sheet.createDrawingPatriarch();
+        ClientAnchor anchor = drawing.createAnchor(0, 0, 0, 0, 0, 5, 15, 20);
+
+        Chart chart = drawing.createChart(anchor);
+        ChartLegend legend = chart.getOrCreateLegend();
+        legend.setPosition(LegendPosition.TOP_RIGHT);
+
+        PieChartData data = new XSSFPieChartData();
+
+        ChartDataSource<String> xs = DataSources.fromStringCellRange(sheet, new CellRangeAddress(0, 0, 0, NUM_OF_COLUMNS - 1));
+        ChartDataSource<Number> ys1 = DataSources.fromNumericCellRange(sheet, new CellRangeAddress(1, 1, 0, NUM_OF_COLUMNS - 1));
+
+        data.addSeries(xs, ys1);
+        chart.plot(data);
+
+        // Write the output to a file
+        FileOutputStream fileOut = new FileOutputStream("/Users/ionut/Downloads/ooxml-pie-chart.xlsx");
+        wb.write(fileOut);
+        fileOut.close();
+    }
+
+    // plotArea.addNewDoughnutChart();
 
     private static int getRandomNumberInRange(int min, int max) {
         Random r = new Random();
